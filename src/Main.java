282e1391eaf496ae200ebdc26fbf33be6156f754
Helloworld <pro.vasiliy.filin@gmail.com>
2023-10-13 19:19:24 +0300

    HW5 done!

--- src/Main.java
+++ src/Main.java
@@ -19,21 +19,17 @@ public class Main {
  */
         System.out.println("Задача 2:");
         var clientDeviceYear = 2015;
-        if (clientDeviceYear < 2015) {
-            if (clientOS == 0) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }
-            else if (clientOS == 1) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
-        else if (clientDeviceYear >= 2015) {
-            if (clientOS == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-            else if (clientOS == 1) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+        else if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
         System.out.println();
 
